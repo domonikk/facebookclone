8656c20334125fd2376488579e3983974cf1e552
domonikk <45376672+domonikk@users.noreply.github.com>
2021-06-01 17:02:15 -0500

    homeupdates

--- facebook-app/src/app/app.module.ts
+++ facebook-app/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { MaterialModule } from './shared/material.module';
+import { MaterialModule } from './shared/material.module';  
 
 @NgModule({
   declarations: [
@@ -15,13 +15,13 @@ import { MaterialModule } from './shared/material.module';
     LoginComponent,
     RegisterComponent, 
     HomeComponent
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule, 
-    FormsModule
+    MaterialModule,  
+    FormsModule, 
+    BrowserAnimationsModule // Browser Animation, Must for pop-up menu
   ],
   providers: [],
   bootstrap: [AppComponent]
